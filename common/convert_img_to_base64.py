8f07fe5c7e0a7dbe50db5ed3cb99b9978e07d116
lspc678 <dltmdqls1945@naver.com>
2021-08-04 19:30:44 +0900

    add comments common dir

--- common/convert_img_to_base64.py
+++ common/convert_img_to_base64.py
@@ -1,6 +1,7 @@
 import base64
 
 
+# 이미지를 base64 코드로 변환
 def convert_img_to_base64_code(img_name):
     with open(img_name, "rb") as img:
         base64_string = base64.b64encode(img.read())  # image --> base64code
--- common/file_manipulation.py
+++ common/file_manipulation.py
@@ -1,5 +1,6 @@
 import os
 
 
+# 파일(이미지) 삭제
 def remove_file(path):
     os.remove(path)
