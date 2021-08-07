7f6639e624e0f7f55330e17e227c20a3c089247c
lspc678 <dltmdqls1945@naver.com>
2021-08-07 17:58:08 +0900

    play music refer to emotion

---
+++ service/play_music.py
@@ -0,0 +1,84 @@
+import os, random, pygame, keyboard
+
+# mp3 재생 설정
+freq = 16000    # frequency
+bitsize = -16   # signed 16 bit. support 8,-8,16,-16s
+channels = 1    # 1 is mono, 2 is stereo
+buffer = 2048   # number of samples (experiment to get right sound)
+
+
+# 음악이 저장된 경로 지정
+music_list_dir = "./music"
+angry_music_list_dir = music_list_dir + "/angry_music/"
+disgust_music_list_dir = music_list_dir + "/disgust_music/"
+fear_music_list_dir = music_list_dir + "/fear_music/"
+happy_music_list_dir = music_list_dir + "/happy_music/"
+neutral_music_list_dir = music_list_dir + "/neutral_music/"
+sad_music_list_dir = music_list_dir + "/sad_music/"
+surprised_music_list_dir = music_list_dir + "/surprised_music/"
+
+# 감정별 음악 리스트
+angry_music_list = os.listdir(angry_music_list_dir)
+disgust_music_list = os.listdir(disgust_music_list_dir)
+fear_music_list = os.listdir(fear_music_list_dir)
+happy_music_list = os.listdir(happy_music_list_dir)
+neutral_music_list = os.listdir(neutral_music_list_dir)
+sad_music_list = os.listdir(sad_music_list_dir)
+surprised_music_list = os.listdir(surprised_music_list_dir)
+
+# 감정 리스트
+emotion_list = ['ANGRY', 'DISGUST', 'FEAR', 'HAPPY', 'NEUTRAL', 'SAD', 'SURPRISE', 'SURPRISED', 'CALM']
+
+
+# 감정에 따른 무작위 음악 선택
+def select_music(emotion):
+    if emotion == "ANGRY":
+        selected_music = random.choice(angry_music_list)
+        print(angry_music_list_dir + selected_music)
+        return angry_music_list_dir + selected_music
+    elif emotion == "DISGUST":
+        selected_music = random.choice(disgust_music_list)
+        print(disgust_music_list_dir + selected_music)
+        return disgust_music_list_dir + selected_music
+    elif emotion == "FEAR":
+        selected_music = random.choice(fear_music_list)
+        print(fear_music_list_dir + selected_music)
+        return fear_music_list_dir + selected_music
+    elif emotion == "HAPPY":
+        selected_music = random.choice(happy_music_list)
+        print(happy_music_list_dir + selected_music)
+        return happy_music_list_dir + selected_music
+    elif emotion == "NEUTRAL" or emotion == "CALM":
+        selected_music = random.choice(neutral_music_list)
+        print(neutral_music_list_dir + selected_music)
+        return neutral_music_list_dir + selected_music
+    elif emotion == "SAD":
+        selected_music = random.choice(sad_music_list)
+        print(sad_music_list_dir + selected_music)
+        return sad_music_list_dir + selected_music
+    elif emotion == "SURPRISE" or emotion == "SURPRISED":
+        selected_music = random.choice(surprised_music_list)
+        print(surprised_music_list_dir + selected_music)
+        return surprised_music_list_dir + selected_music
+    else:
+        print("Invalid argument. Parameter Name: emotion")
+
+
+def music_play(emotion):
+    if emotion in emotion_list:
+        selected_music = select_music(emotion)
+        pygame.mixer.init(freq, bitsize, channels, buffer)
+        pygame.mixer.music.load(selected_music)
+        pygame.mixer.music.play()
+
+        clock = pygame.time.Clock()
+        while pygame.mixer.music.get_busy():  # 음악 재생
+            clock.tick(60)
+
+            if pygame.mixer.music.get_pos() > 60000:  # 60초 지나면 음악이 멈춤
+                break
+
+        pygame.mixer.quit()
+
+
+music_play("HAPPY")
