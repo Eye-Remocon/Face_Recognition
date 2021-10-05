3c2b8e92fa161972f250ce72d0e52f4cf06ee98b
leeseunghui <hithere1012@naver.com>
2021-10-05 17:36:56 +0900

    change pose detection service code

--- service/pose_detection.py
+++ service/pose_detection.py
@@ -1,3 +1,5 @@
+#-*-coding: utf-8-*-
+#-*-coding: euc-kr-*-
 import requests
 import base64
 
@@ -14,19 +16,18 @@ def check(ip):
 def get_pose(img_name, ip):
     # 저장된 캡쳐 이미지 불러온 후base64코드로 인코드
     with open(img_name, 'rb') as f:
-        im_b64 = base64.b64encode(f.read())
-
+        im_b64 = base64.b64encode(f.read()).decode('utf8')
     # 이미지 payload 형태로 만든 후 flask 서버에 post 요청
-    payload = {'image': im_b64}
+    payload = {'img_base64': im_b64}
     headers = {}
-    url = 'http://' + ip + ':3500/pose_detection'
+    url2 = 'http://'+ip+':3500/pose_detection'
 
-    r = requests.post(url, json=payload, headers=headers)
+    r = requests.post(url2, json=payload, headers=headers)
 
     if r.ok:
         pose = r.json()
-        if pose[1]['probability'] > 0.8:
-            return result.append(pose[0]['className'])
+        if pose[0]['probability'] > 0.8:
+            return pose[0]['className']
     return 'none'
 
 
