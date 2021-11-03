7e09299c3d01695470059e878d39fffd6c5a5e83
lspc678 <dltmdqls1945@naver.com>
2021-11-03 04:16:19 -0700

    add resize frame

--- camera/capture_image_by_webcam.py
+++ camera/capture_image_by_webcam.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2, imutils
 
 
 # 윈도우 이름 설정
@@ -9,19 +9,17 @@ def set_window_name(window_name):
 # 프레임 단위로 카메라 장면을 화면에 나타냄
 def show_camera_frame(video_capture, window_name):
     ret, frame = video_capture.read()
+    
+    frame = imutils.resize(frame, width=1024, height=768)
 
     if ret:
         cv2.imshow(window_name, frame)
 
 
 # 사진 촬영
-def do_video_capture(video_capture, window_name, save_img_name):
+def do_video_capture(video_capture, save_img_name):
     # video_capture = cv2.VideoCapture(0)
     ret, frame = video_capture.read()
-
-    if ret:
-        cv2.imshow(window_name, frame)
-
     save_img(save_img_name, frame)
 
 
