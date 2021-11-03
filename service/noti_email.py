8925fa1f94d8209e7d3c45a494bf514f0dbd1047
lspc678 <dltmdqls1945@naver.com>
2021-11-03 04:06:52 -0700

    final noti_email.py

--- service/noti_email.py
+++ service/noti_email.py
@@ -2,6 +2,7 @@ import smtplib, base64
 from email.mime.text import MIMEText
 
 smtp_user = ""  # 입력된 "이메일:비밀번호"를 base64 형식으로 인코딩
+# ex) 이메일이 "abcd1234@naver.com", 비밀번호가 "12345678"이라면 smtp_user = "YWJjZDEyMzRAbmF2ZXIuY29tOjEyMzQ1Njc4"
 smtp_user_decode = base64.b64decode(smtp_user).decode("ASCII")  # base64를 디코딩을 통해 이메일:비밀번호 형식으로 변환
 
 smtp_user_id = smtp_user_decode.split(":")[0]
@@ -13,8 +14,8 @@ smtp_info = dict({"smtp_server": "smtp.naver.com",  # SMTP 서버 주소
                   "smtp_port": 587})  # SMTP 서버 포트
 
 # 메일 내용 작성
-title = "python으로 이메일 보내기" # 예시 문구
-content = "python으로 이메일 보내기 테스트" # 예시 문구
+title = "긴급 상황 발생" # 예시 문구
+content = "긴급 상황이 발생했습니다. 즉시 확인해 주시기 바랍니다." # 예시 문구
 sender = "" # 보내는 사람의 이메일 주소
 receiver = "" # 받는 사람의 이메일 주소
 
@@ -26,21 +27,24 @@ msg['From'] = sender       # 송신자
 msg['To'] = receiver       # 수신자
 
 
-def send_email(smtp_info, msg):
+def send_email():
+    smtp_info = dict({"smtp_server": "smtp.naver.com",  # SMTP 서버 주소
+                  "smtp_user_id": smtp_user_id,
+                  "smtp_user_pw": smtp_user_pw,
+                  "smtp_port": 587})  # SMTP 서버 포트
+    
+    msg = MIMEText(_text=content, _charset="utf-8")  # 메일 내용
+
+    msg['Subject'] = title     # 메일 제목
+    msg['From'] = sender       # 송신자
+    msg['To'] = receiver       # 수신자
+    
     with smtplib.SMTP(smtp_info["smtp_server"], smtp_info["smtp_port"]) as server:
-        # TLS 보안 연결
         server.starttls()
-        # 로그인
         server.login(smtp_info["smtp_user_id"], smtp_info["smtp_user_pw"])
-
-        # 로그인 된 서버에 이메일 전송
-        response = server.sendmail(msg['from'], msg['to'], msg.as_string())  # 메시지를 보낼때는 .as_string() 메소드를 사용해서 문자열로 바꿔줍니다.
-
-        # 이메일을 성공적으로 보내면 결과는 {}
+        response = server.sendmail(msg['from'], msg['to'], msg.as_string())
+        
         if not response:
             print('이메일을 성공적으로 보냈습니다.')
         else:
             print(response)
-
-
-send_email(smtp_info, msg)
